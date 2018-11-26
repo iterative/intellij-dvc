fc0740445b56088ab111976dbc668bfb605f1af0
prihoda <david.prihoda@gmail.com>
2018-11-26 15:42:59 +0100

    Bash syntax highlighting.

--- README.md
+++ README.md
@@ -8,6 +8,8 @@ See <a href="http://dvc.org">DVC Website</a> and <a href="https://github.com/ite
 
 **Features:**
 
+- Bash syntax highlighting in cmd field (when <a href="https://plugins.jetbrains.com/plugin/4230-bashsupport">BashSupport</a> plugin is installed)
+    - Known issue: Fails for multi-line commands
 - Click-through file references for dependencies and outputs (ctrl+click)
 - DVC file syntax highlighting (YAML)
 - DVC file icon
---
+++ src/main/java/com/davidprihoda/dvc/DvcCommandLanguageInjector.java
@@ -0,0 +1,44 @@
+package com.davidprihoda.dvc;
+
+import com.ansorgit.plugins.bash.file.BashFileType;
+import com.intellij.openapi.util.TextRange;
+import com.intellij.psi.ElementManipulators;
+import com.intellij.psi.InjectedLanguagePlaces;
+import com.intellij.psi.LanguageInjector;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiLanguageInjectionHost;
+import org.jetbrains.annotations.NotNull;
+import com.intellij.openapi.diagnostic.Logger;
+import org.jetbrains.yaml.psi.impl.YAMLCompoundValueImpl;
+import org.jetbrains.yaml.psi.impl.YAMLKeyValueImpl;
+
+public class DvcCommandLanguageInjector implements LanguageInjector {
+    private static final Logger LOG = Logger.getInstance(DvcCommandLanguageInjector.class);
+
+    @Override
+    public void getLanguagesToInject(@NotNull PsiLanguageInjectionHost host, @NotNull InjectedLanguagePlaces injectionPlacesRegistrar) {
+        if(host.getContainingFile() == null ||
+                host.getContainingFile().getVirtualFile() == null ||
+                host.getContainingFile().getVirtualFile().getFileType() != DvcFileType.INSTANCE) {
+            return;
+        }
+        PsiElement parent = host.getParent();
+//        LOG.info("Parent of: '"+host.getText()+"' ("+host.getClass().getSimpleName()+") is '"+parent.getText()+"' ("+parent.getClass().getSimpleName()+")");
+//        if (parent instanceof YAMLCompoundValueImpl) {
+//            parent = parent.getParent();
+//            LOG.info("Moving parent to: '"+parent.getText()+"'");
+//        }
+        if (parent instanceof YAMLKeyValueImpl) {
+            YAMLKeyValueImpl keyValue = (YAMLKeyValueImpl)parent;
+            //LOG.info("Key value: "+keyValue.getValue());
+            if(keyValue.getKeyText().equals("cmd")) {
+                TextRange contentRange = ElementManipulators.getValueTextRange(host);
+                //LOG.info("Adding range: "+contentRange);
+                if (!contentRange.isEmpty()) {
+                    injectionPlacesRegistrar.addPlace(BashFileType.BASH_LANGUAGE, contentRange, null, null);
+                }
+            }
+        }
+
+    }
+}
