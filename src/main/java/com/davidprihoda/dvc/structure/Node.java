a8cc55ee89a1aedbce7749b694c7bfbe99e095a5
Tomas Caithaml <tomas.caithaml@merck.com>
2019-03-14 17:30:05 +0100

    Add DVC Structure Tool window with hardcoded file structure.

---
+++ src/main/java/com/davidprihoda/dvc/structure/Node.java
@@ -0,0 +1,13 @@
+package com.davidprihoda.dvc.structure;
+
+public class Node {
+    private final String text;
+
+    public Node(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+}
